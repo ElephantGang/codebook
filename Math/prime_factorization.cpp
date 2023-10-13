8fa83f56a21fd759af22f1ce623a1e30d539df5d
Ststone <chima.miro@gmail.com>
2023-10-13 11:38:29 +0800

    simplify version

--- Math/prime_factorization.cpp
+++ Math/prime_factorization.cpp
@@ -6,41 +6,6 @@ vector<ll> vv;
 ll abs(ll x){
     return (x>0?x:-x);
 }
-ll power(ll x,ll y,ll p){
-    ll res = 1;
-    x = x % p;
-    while (y > 0){
-        if (y & 1)
-            res = (res*x) % p;
-        y = y>>1;
-        x = (x*x) % p;
-    }
-    return res;
-}
-bool miillerTest(ll d, ll n){
-    ll a = 2 + rand () % (n - 4);
-    ll x = power(a, d, n);
-    if (x == 1  || x == n-1)
-        return true ;
-    while (d != n-1){
-        x = (x * x) % n;
-        d *= 2;
-        if (x == 1) return false ;
-        if (x == n-1) return true ;
-    }
-    return false ;
-}
-bool isPrime(ll n, ll k){
-    if (n <= 1 || n == 4) return false ;
-    if (n <= 3) return true ;
-    ll d = n - 1;
-    while (d % 2 == 0)
-        d /= 2;
-    for (ll i = 0; i < k; i++)
-        if (!miillerTest(d, n))
-            return false ;
-    return true ;
-}
 ll func(ll t,ll c,ll x) {
 	return (t*t+c)%x;
 }
@@ -75,7 +40,7 @@ void prefactor(ll &n, vector<ll> &v) {
 	}
 }
 void comfactor(const ll &n, vector<ll> &v) {
-	if(isPrime(n,15)) {
+	if(isPrime(n,15)) { // MillerRabin
 		v.push_back(n);
 		return;
 	}
