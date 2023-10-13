8da42ca4ad0cad54f71d4c1c0a7c8bd9aed790c8
Koying <koyingchen0523@gmail.com>
2023-10-13 10:54:35 +0800

    delete 2D BIT, range segmenttree and modify BIT

--- Data_Structure/FenwickTree.cpp
+++ Data_Structure/FenwickTree.cpp
@@ -1,49 +1,24 @@
 /** 普通 BIT ，為了加速打字只支援 1-based **/
-const int maxn = ? ;  // 開全域加速打字
-class BIT {
-   private:
-    ll a[maxn];
-    ll sum(int i) {
-        ll r = 0;
-        while (i > 0) r += a[i], i -= i & -i;
-        return r;
+const int MAXN = ? ;  // 開全域加速打字
+#define lowbit(x & (-x))
+
+
+template<typename T>
+struct Fenwick_Tree { // 1 based
+    // 二維：陣列開二維，修改跟查詢就是對 (x, y) 各自 +- lowbit
+    T arr[MAXN];
+    void init(int _n = MAXN) {
+        for (int i = 0; i < _n; i++)
+            arr[i] = 0;
     }
-   public:
-    // size = maxn 的空 BIT ，所有元素都是零
-    BIT() { memset(a, 0, sizeof(a)); }
-    // 注意 1-based
-    void add(int i, ll v) {
-        while (i < maxn) a[i] += v, i += i & -i;
+    void update(int i, T val) {
+        for (; i < MAXN; i += lowbit(i))
+            arr[i] += val;
     }
-    // 注意 1-based
-    ll sum(int l, int r) { return sum(r) - sum(l - 1); }
-};
-/** 區間加值 BIT，只支援 1-based。複雜度 O(Q*log(N)) **/
-const int maxn = ? ;  // 開全域加速打字
-class RangeUpdateBIT {
-   private:
-    ll d[maxn], dd[maxn];
-    ll sum(int i) {
-        ll s = 0, ss = 0;
-        int c = i + 1;  // 這行不是打錯！要加！
-        while (i > 0) s += d[i], ss += dd[i], i -= i & -i;
-        return c * s - ss;
-    }
-    void add(int i, ll v) {
-        int c = i;
-        while (i < maxn)
-            d[i] += v, dd[i] += c * v, i += i & -i;
-    }
-   public:
-    // 空 BIT，size = maxn，所有元素都是零，注意 1-based
-    RangeUpdateBIT() {
-        memset(d, 0, sizeof(d));
-        memset(dd, 0, sizeof(dd));
-    }
-    // 必區間區間求和，注意 1-based
-    ll sum(int l, int r) { return sum(r) - sum(l - 1); }
-    // 必區間區間加值，注意 1-based
-    void add(int l, int r, ll v) {
-        add(l, v), add(r + 1, -v);
+    T query(int i) {
+        T ret = 0;
+        for (; i; i -= lowbit(i))
+            ret += arr[i];
+        return ret;
     }
 };
