a52acecf6f5c68fb82922e15bd9f17bc5d7d9eac
Ststone <chima.miro@gmail.com>
2023-10-13 11:39:44 +0800

    delete fastpow

--- Math/MillerRabin.cpp
+++ Math/MillerRabin.cpp
@@ -3,14 +3,6 @@ inline LL mul(LL a,LL b,LL m){//a*b%m
     return (a%m)*(b%m)%m;
 }
 
-template<typename T> T pow(T a,T b,T mod) { //a^b%mod
-    T ans = 1;
-    while(b) {
-        if(b&1) ans = mul(ans,a,mod);
-        a = mul(a,a,mod);
-        b >>= 1;
-    } return ans;
-}
 template<typename T> bool isprime(T n, int num=3) { //num = 3,7
     int sprp[3] = {2,7,61}; //int範圍可解
     //int llsprp[7] = {2,325,9375,28178,450775,9780504,1795265022}; //至少unsigned long long範圍
@@ -22,7 +14,7 @@ template<typename T> bool isprime(T n, int num=3) { //num = 3,7
     for(int i=0; i<num; i++) {
         T a = sprp[i]%n;
         if(a==0 || a==1 || a==n-1) continue;
-        T x = pow(a,u,n);
+        T x = fpow(a,u,n);
         if(x==1 || x==n-1) continue;
         for(int j=1; j<t; j++) {
             x = mul(x,x,n);
